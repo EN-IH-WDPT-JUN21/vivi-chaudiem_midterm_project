726767cab7938db555355176ca335c88bc2adbef
vivi-chaudiem <nguyen.chaudiem@yahoo.de>
2021-09-26 16:57:07 +0200

    Fixed problem with address not displaying correctly

--- src/main/java/com/ironhack/midtermproject/dao/Address.java
+++ src/main/java/com/ironhack/midtermproject/dao/Address.java
@@ -1,8 +1,17 @@
 package com.ironhack.midtermproject.dao;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Embeddable;
 
 @Embeddable
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Address {
     private String streetAddress;
     private String postalCode;
